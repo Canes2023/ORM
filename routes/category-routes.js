8773cb42b23030fad7abd781f4854fe31fd1f8cb
Canes2023 <dxc1449@miami.edu>
2024-01-15 16:01:08 -0500

    Fixed issue where localhost wouldn't run on port 3001

---
+++ routes/category-routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { Category } = require('../models');
+
+// Define CRUD routes using Sequelize models
+// ...
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware and other configurations
+// ...
+
+// Use routes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
